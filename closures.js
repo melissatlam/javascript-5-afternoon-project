36e4e93faff937b26f4a181c95b84a3398afc5fa
Melissa <Melissalammm@gmail.com>
2020-04-15 08:56:55 -0500

    finished

--- closures.js
+++ closures.js
@@ -23,14 +23,14 @@ function outer() {
 */
   
 // Code Here
-
+let inner = outer();
 
 
 //Once you do that, invoke inner.
 
 //Code Here
 
-
+inner();
 
 ////////// PROBLEM 2 //////////
 
@@ -53,6 +53,10 @@ function callFriend(name) {
 
 //Code Here
 
+let callJake = callFriend('Jake');
+callJake(435-555-9248);
+
+
 
 
 ////////// PROBLEM 3 //////////
@@ -63,16 +67,18 @@ function callFriend(name) {
 
 //Code Here
 
+function makeCounter(){
+  let count = 0
+
+  function addOne() {
+    return count += 1;
+};
+return addOne;
+};
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
+// Uncomment this once you make your function
+// 
 
 ////////// PROBLEM 4 //////////
 
@@ -85,19 +91,24 @@ function callFriend(name) {
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
-function counterFactory(value) {
-  // Code here.
-
+function counterFactory (counter) {
   return {
-
-  };
+    inc: function() {
+      counter = counter + 1;
+      return counter;
+    },
+    dec: function() {
+      counter = counter -1
+      return counter;
+    },
+  }
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +124,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message(Billy, Bob) {
+    return welcomeText + ' ' + firstname + ' ' + lastname + '.'
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,7 +157,11 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: ()=>{
+      return privateMethod() 
+    }
   };
+  module.publicMethod();
 })();
 
 
@@ -162,7 +179,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: (num) => {
+      updatedNum = secret += num
+      return updatedNum;
+    },
+    takeAwayFromSecret: (num) => {
+      updatedNum = secret -= num
+      return updatedNum;
+    }// Code here
   };
 }
 
@@ -194,3 +218,4 @@ function timeOutCounter() {
   }
 }
 timeOutCounter();
+
